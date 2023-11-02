9662f7bf9a76f14c737f614c5b0cd4ce06b4801c
Archisha Dhyani <114275146+Archisha-Dhyani@users.noreply.github.com>
2023-11-03 00:21:30 +0530

    Create experiment.js

---
+++ Day 04/experiment.js
@@ -0,0 +1,30 @@
+console.log("hello World");
+// var 
+var a=10;
+var a=60;
+console.log(a); 
+// redeclare as well as update 
+
+// let
+let b=10;
+b=90;
+console.log(b);
+// we can update but cant redeclare 
+
+// const
+const c=100;
+ c=80;
+  console.log(c);
+// none 
+
+console.log(d);
+var d=90;
+console.log(d);
+ // hoisting 
+
+hello();
+function hello(){
+    console.log("hello");
+}
+
+// hoisting 
